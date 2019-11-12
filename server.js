2b610db45261350220e89d2bed4f48f8e748471f
Matt Lorber <matthew.lorber@outlook.com>
2019-11-12 06:45:53 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express");
-var db = require("./models");
+var db = require("./app/models");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app/public"));
+
 db.sequelize.sync().then(() => {
 	app.listen(PORT, () => {
-		console.log("Listening on %s", PORT);
+		console.log("Listening on port %s", PORT);
 	});
 });
