2f5e3f3798e2b4d7e425d02af759d81ac855b2e2
Matt Lorber <matthew.lorber@outlook.com>
2019-11-12 07:13:02 -0500

    added api routes, production db, assets

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("app/public"));
 
+require("./app/routes/api-routes.js")(app);
+
 db.sequelize.sync().then(() => {
 	app.listen(PORT, () => {
 		console.log("Listening on port %s", PORT);
