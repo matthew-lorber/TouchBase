1a7f63d48e2f075246e2838fe1700a4002781344
Matt Lorber <matthew.lorber@outlook.com>
2019-11-12 20:23:33 -0500

    updated config

--- app/config/config.js
+++ app/config/config.js
@@ -17,11 +17,17 @@ module.exports = {
     },
     "production": {
         // "use_env_variable": "JAWSDB_URL",
-        "use_env_variable": "mysql://sxwl53txwa01eraj:qxc2ueu1oqs0rn2t@lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c8uqfusf09qdnwxt",
-        "username": process.env.MYSQL_UN,
-        "password": process.env.MYSQL_KEY,
-        "database": process.env.MYSQL_DB,
-        "host": process.env.MYSQL_HOST,
-        "dialect": "mysql"
+        // "use_env_variable": "mysql://sxwl53txwa01eraj:qxc2ueu1oqs0rn2t@lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c8uqfusf09qdnwxt",
+        // "username": process.env.MYSQL_UN,
+        // "password": process.env.MYSQL_KEY,
+        // "database": process.env.MYSQL_DB,
+        // "host": process.env.MYSQL_HOST,
+        "dialect": "mysql",
+        "username": "sxwl53txwa01eraj",
+    "password": "qxc2ueu1oqs0rn2t",
+    "database": "c8uqfusf09qdnwxt",
+    "host": "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql",
+    "operatorsAliases": false
     }
 }
